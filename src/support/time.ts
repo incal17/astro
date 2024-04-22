61db21eb022819c5d0bf37da56d4479da1cc94ea
Godruoyi <godruoyi@gmail.com>
2024-04-22 17:51:32 +0800

    wip

---
+++ src/support/time.ts
@@ -0,0 +1,7 @@
+function formatDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' }
+
+    return new Date(date).toLocaleDateString(undefined, options)
+}
+
+export { formatDate }
