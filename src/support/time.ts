b38a4afd910a28662752d559c9a924453a74badc
Godruoyi <godruoyi@gmail.com>
2024-05-02 12:14:53 +0800

    chore: finish timeline

--- src/support/time.ts
+++ src/support/time.ts
@@ -18,9 +18,11 @@ const TimeAgoConfiguration: string[][] = [
 ]
 
 function formatDate(date: Date): string {
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' }
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
 
-    return new Date(date).toLocaleDateString(undefined, options)
+    return `${year}/${month}/${day}`
 }
 
 function timeago(date?: Date): string {
