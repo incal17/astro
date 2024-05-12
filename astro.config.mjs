f7c13711ae75419af39f52bc90809e8fab5d89f2
Godruoyi <godruoyi@gmail.com>
2024-05-12 16:19:19 +0800

    feat: compress & partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,21 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
-import compressor from 'astro-compressor'
+import partytown from '@astrojs/partytown'
 import { SITE } from './src/config.ts'
 import { remarkReadingTime } from './src/support/time.ts'
 
 export default defineConfig({
     site: SITE.url,
     image: {},
-    integrations: [mdx(), sitemap(), tailwind(), react(), compressor({ gzip: false, brotli: true })],
+    integrations: [
+        mdx(),
+        sitemap(),
+        tailwind(),
+        react(),
+        partytown(),
+        (await import('@playform/compress')).default(),
+    ],
     markdown: {
         remarkPlugins: [remarkReadingTime],
         shikiConfig: {
