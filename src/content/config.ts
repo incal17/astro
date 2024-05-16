5b7797d1cd1e6984ce31e405e22ef443a3edf9fa
Godruoyi <godruoyi@gmail.com>
2024-05-16 14:02:12 +0800

    chore: refactor and new blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,30 @@
 import { defineCollection, z } from 'astro:content'
-// import { Categories } from '../config.ts'
-// const slugs = Categories.map(c => c.slug)
-// const categories = z.enum(slugs as [string, ...string[]])
 
 const posts = defineCollection({
     schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
-        cardImage: image(),
-        cardImage2: image().optional(),
-        ogImage: image()
-            .refine(img => img.width >= 1200 && img.height >= 630, {
-                message: 'OpenGraph image must be at least 1200 X 630 pixels!',
-            })
-            .or(z.string())
-            .optional(),
+        banner: image(),
+
+        // This banner will be shown in blog lists(/posts) if provided.
+        banner2: image().optional(),
+
+        // The article OG cover, if not provided, use summary card, otherwise summary_large_image
+        ogImage: image().refine(img => img.width >= 1200 && img.height >= 630, {
+            message: 'OpenGraph image must be at least 1200 X 630 pixels!',
+        }).or(z.string()).optional(),
+
         category: z.string(),
         pubDate: z.coerce.date(),
+
+        // Should the article be added to SELECTED POSTS? will be displayed on the /posts page if true.
         selected: z.boolean().optional(),
-        oldViewCount: z.number().optional(),
+
         tags: z.array(z.string()).optional(),
+
+        // not use, just record this value since its from my previous blog system
         updatedDate: z.coerce.date().optional(),
+        oldViewCount: z.number().optional(),
     }),
 })
 
@@ -29,7 +33,6 @@ const categoryCollection = defineCollection({
     schema: () => z.object({
         title: z.string(),
         description: z.string(),
-        icon: z.string().optional(),
     }),
 })
 
